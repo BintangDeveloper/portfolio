b0b3be8d1d7be2a6aa344abe821b63ea0f742409
bintangdeveloper <bintangdeveloper@gmail.com>
2024-06-21 15:28:03 +0700

    +

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import AboutSection from "@/components/AboutSection";
@@ -8,6 +9,10 @@ import AchievementsSection from "@/components/AchievementsSection";
 export default function Index() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Head>
+        <title>BintangDeveloper | Portfolio</title>
+        <meta name="description" content="Portfolio page for BintangDeveloper" />
+      </Head>
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
