95862d2d5307adee29bcf67393630da295492dcb
bintangdeveloper <bintangdeveloper@gmail.com>
2024-07-13 16:52:05 +0700

    +

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const LoadingScreen: React.FC = () => {
   return (
     <div className="loading fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black z-50">
-      <div className="ring">
+      <div className="e7dc0d49636f4194266ce34b5322a2861b7fb1bf">
         <div></div>
         <div></div>
         <div></div>
