ce004ce131d0135e8340effe85c92b3b019e5e0b
bintangdeveloper <bintangdeveloper@gmail.com>
2024-10-09 10:15:19 +0700

    +

--- components/sosial-buttons.tsx
+++ components/sosial-buttons.tsx
@@ -11,22 +11,23 @@ type SosialButtonsProps = {
   social: Record<string, SocialItem>;
 };
 
-const SosialButtons: React.FC<SosialButtonsProps> = ({ social }) => {
+export function SosialButtons({ social }: SosialButtonsProps) {
   return (
     <div className="py-8 flex items-center justify-center gap-2 flex-wrap">
-      {Object.keys(social).map((key) => (
-        <a
-          key={key}
-          href={social[key].url}
-          className="p-2 rounded-lg flex items-center border border-gray-300 justify-center transition-all duration-500 hover:border-gray-100 hover:bg-gray-100"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <social[key].icon width="28" height="28" />
-        </a>
-      ))}
+      {Object.keys(social).map((key) => {
+        const Icon = social[key].icon;
+        return (
+          <a
+            key={key}
+            href={social[key].url}
+            className="p-2 rounded-lg flex items-center border border-gray-300 justify-center transition-all duration-500 hover:border-gray-100 hover:bg-gray-100"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon width="28" height="28" />
+          </a>
+        );
+      })}
     </div>
   );
-};
-
-export default SosialButtons;
+}
