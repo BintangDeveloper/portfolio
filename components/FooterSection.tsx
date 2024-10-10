fd7faaed706cb6e9380b446b9f0d62d7d601dc46
bintangdeveloper <bintangdeveloper@gmail.com>
2024-10-10 09:58:11 +0700

    +

--- components/FooterSection.tsx
+++ components/FooterSection.tsx
@@ -29,7 +29,7 @@ export default function FooterSection({ social }: SosialButtonsProps) {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  {social[key].icon}
+                  <Icon />
                 </a>
               );
             })}
